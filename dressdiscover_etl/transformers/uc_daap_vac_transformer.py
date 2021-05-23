b1627e2b2e5a65591b728156d62ca8cff5275a60
Minor G <minorg@server.fake>
2021-05-23 09:57:01 -0400

    adapt UcDaapVacTransformer to paradicms

--- dressdiscover_etl/transformers/uc_daap_vac_transformer.py
+++ dressdiscover_etl/transformers/uc_daap_vac_transformer.py
@@ -1,23 +1,22 @@
 import dataclasses
 
+from paradicms_etl.models.creative_commons_licenses import CreativeCommonsLicenses
 from paradicms_etl.models.rights import Rights
-from paradicms_etl.models.rights_value import RightsValue
 from paradicms_etl.transformers.luna_transformer import LunaTransformer
 
 
 class UcDaapVacTransformer(LunaTransformer):
+    def transform(self, **kwds):
+        yield CreativeCommonsLicenses.BY_NC_ND
+        yield from LunaTransformer.transform(**kwds)
+
     def _transform_institution(self, **kwds):
         institution = LunaTransformer._transform_institution(self, **kwds)
         assert institution.rights is None
         return dataclasses.replace(
             institution,
             rights=Rights(
-                license=RightsValue(
-                    text="Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License",
-                    uri="http://creativecommons.org/licenses/by-nc-nd/4.0/",
-                ),
-                statement=RightsValue(
-                    text="Copyright University of Cincinnati Libraries"
-                ),
+                license=CreativeCommonsLicenses.BY_NC_ND.uri,
+                statement="Copyright University of Cincinnati Libraries",
             ),
         )
