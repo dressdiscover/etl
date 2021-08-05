f5e4b57654e21d78dbc3471764f3d6ae9fd1befa
Minor G <minorg@server.fake>
2021-08-04 20:21:07 -0400

    fix install_requirements.bat

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 cd %~dp0
 CALL venv\Scripts\activate.bat
-pip install -r ..\..\paradicms\etl\requirements.txt
-pip install -r ..\..\pastpy\requirements.txt
-pip install spacy textacy
+pip install ..\paradicms\etl
+pip install -r ..\pastpy\requirements.txt
